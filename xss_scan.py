20f5a517e17f61432aa7baf8fd0985b2855a0e32
iam-JD <118123756+iamone-JD@users.noreply.github.com>
2024-08-16 22:50:56 -0400

    Update xss_scan.py

--- xss_scan.py
+++ xss_scan.py
@@ -3,23 +3,26 @@ import urllib.parse
 import argparse
 
 def text_xss(url):
-    payload = {'input': '<script>alert("XSS")</script>'}
+    payload = '<script>alert("XSS")</script>'
     try:
         # Check if the URL is valid
         result = urllib.parse.urlparse(url)
         if not all([result.scheme, result.netloc]):
             raise ValueError("Invalid URL")
-
+        #To use it you have to change the session cookie for yours
+        cookies = {
+            "PHPSESSID":"2acijd30vevs4hvhmbdefq42t7",
+            "security":"low"
+        }
         # Send the request
-        response = requests.post(url, data=payload, timeout=5)
+        response = requests.get(str(url) + str(payload), cookies=cookies, timeout=5)
         response.raise_for_status()  # Raise an exception for HTTP errors
-
         # Check if the payload is reflected in the response
-        if payload['input'] in response.text:
+        if payload in response.text:
             print(f"Vulnerable to XSS at {url}")
         else:
             print(f"Not vulnerable to XSS at {url}")
-
+            print(response.text)
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
     except requests.exceptions.ConnectionError as conn_err:
@@ -32,8 +35,6 @@ def text_xss(url):
         print(f"Invalid URL: {val_err}")
     except Exception as err:
         print(f"An unexpected error occurred: {err}")
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script para escanear XSS')
